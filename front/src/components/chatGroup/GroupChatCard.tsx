428a5335180de52ab0e18217da34f9af604cc921
unknown <shashidhar4449@gmail.com>
2025-04-26 12:03:57 -0600

    group chat card

--- front/src/components/chatGroup/GroupChatCard.tsx
+++ front/src/components/chatGroup/GroupChatCard.tsx
@@ -1,31 +1,29 @@
-import { CustomUser } from '@/app/api/auth/[...nextauth]/options';
-import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
-import React from 'react';
-import GroupChatCardMenu from './GroupChatCardMenu';
+import React from "react";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { CustomUser } from "@/app/api/auth/[...nextauth]/options";
+import GroupChatCardMenu from "./GroupChatCardMenu";
+import Link from "next/link";
+import Env from "@/lib/env";
 
 export default function GroupChatCard({
-	group,
-	user,
+  group,
+  user,
 }: {
-	group: GroupChatType;
-	user: CustomUser;
+  group: GroupChatType;
+  user: CustomUser;
 }) {
-	return (
-		<Card>
-			<CardHeader className="flex justify-between items-center ">
-				<CardTitle className="text-2xl">{group.title}</CardTitle>
-				<GroupChatCardMenu user={user} group={group} />
-			</CardHeader>
-			<CardContent>
-				<p className="text-xl text-gray-500">
-					Passcode :{' '}
-					<strong className="text-xl text-black">{group.passcode}</strong>
-				</p>
-				{/* <br /> */}
-				<p className="text-xl text-gray-500">
-					Created At : {new Date(group.created_at).toDateString()}
-				</p>
-			</CardContent>
-		</Card>
-	);
+  return (
+    <Card>
+      <CardHeader className="flex-row justify-between items-center ">
+        <CardTitle className="text-2xl">{group.title}</CardTitle>
+        <GroupChatCardMenu user={user} group={group} />
+      </CardHeader>
+      <CardContent>
+        <p>
+          Passcode :-<strong>{group.passcode}</strong>
+        </p>
+        <p>Created At :-{new Date(group.created_at).toDateString()}</p>
+      </CardContent>
+    </Card>
+  );
 }
