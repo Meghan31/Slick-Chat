941797b9d6c38ee18d65671161932fb0167c1a29
srinath-19 <srinath.m1902@gmail.com>
2025-04-26 11:29:09 -0600

    index

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,18 +1,26 @@
-import { Router } from 'express';
-import AuthController from '../controllers/AuthController.js';
-import ChatGroupController from '../controllers/ChatGroupController.js';
-import authMiddleware from '../middlewares/AuthMiddleware.js';
-
+import { Router } from "express";
 const router = Router();
+import AuthController from "../controllers/AuthController.js";
+import ChatGroupController from "../controllers/ChatGroupController.js";
+import authMiddleware from "../middleware/AuthMiddleware.js";
+import ChatGroupUserController from "../controllers/ChatGroupUserController.js";
+import ChatsController from "../controllers/ChatsController.js";
 
-// Auth routes
-router.post('/auth/login', AuthController.login);
+// Auth Routes
+router.post("/auth/login", AuthController.login);
 
-// chat group routes
-router.post('/chat-group', authMiddleware, ChatGroupController.store);
-router.get('/chat-group', authMiddleware, ChatGroupController.index);
-router.get('/chat-group/:id', authMiddleware, ChatGroupController.show);
-router.put('/chat-group/:id', authMiddleware, ChatGroupController.update);
-router.delete('/chat-group/:id', authMiddleware, ChatGroupController.destroy);
+// Chat Group Routes
+router.get("/chat-group", authMiddleware, ChatGroupController.index);
+router.get("/chat-group/:id", ChatGroupController.show);
+router.post("/chat-group", authMiddleware, ChatGroupController.store);
+router.put("/chat-group/:id", authMiddleware, ChatGroupController.update);
+router.delete("/chat-group/:id", authMiddleware, ChatGroupController.destroy);
+
+// * Chat group user
+router.get("/chat-group-user", ChatGroupUserController.index);
+router.post("/chat-group-user", ChatGroupUserController.store);
+
+// * Chats
+router.get("/chats/:groupId", ChatsController.index);
 
 export default router;
