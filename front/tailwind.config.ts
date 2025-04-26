327c3cc941ae881727856edf8c9e5e7eedeeae33
unknown <shashidhar4449@gmail.com>
2025-04-26 11:57:36 -0600

    tailwind configuration

--- front/tailwind.config.ts
+++ front/tailwind.config.ts
@@ -1,66 +1,80 @@
-// import prisma from "./config/db.config.js";
-// import { producer, consumer } from "./config/kafka.config.js";
+import type { Config } from "tailwindcss"
 
-// export const produceMessage = async (topic: string, message: any) => {
-//   await producer.send({
-//     topic,
-//     messages: [{ value: JSON.stringify(message) }],
-//   });
-// };
-
-// export const consumeMessages = async (topic: string) => {
-//   await consumer.connect();
-//   await consumer.subscribe({ topic: topic });
-
-//   await consumer.run({
-//     eachMessage: async ({ topic, partition, message }) => {
-//       const data = JSON.parse(message.value.toString());
-//       console.log({
-//         partition,
-//         offset: message.offset,
-//         value: data,
-//       });
-
-//       await prisma.chats.create({
-//         data: data,
-//       });
-
-//       // Process the message (e.g., save to DB, trigger some action, etc.)
-//     },
-//   });
-// };
-
-
-
-import prisma from "./config/db.config.js";
-import { producer, consumer } from "./config/kafka.config.js";
-
-export const produceMessage = async (topic: string, message: any) => {
-  await producer.send({
-    topic,
-    messages: [{ value: JSON.stringify(message) }],
-  });
-};
-
-export const consumeMessages = async (topic: string) => {
-  await consumer.connect();
-  await consumer.subscribe({ topic });
-
-  await consumer.run({
-    eachMessage: async ({ topic, partition, message }) => {
-      const data = JSON.parse(message.value.toString());
-      console.log({
-        partition,
-        offset: message.offset,
-        value: data,
-      });
-
-      // Save message to the database
-      await prisma.chats.create({
-        data: data,
-      });
-
-      // Process the message (e.g., save to DB, trigger some action, etc.)
+const config = {
+  darkMode: ["class"],
+  content: [
+    './pages//*.{ts,tsx}',
+    './components//*.{ts,tsx}',
+    './app//*.{ts,tsx}',
+    './src//*.{ts,tsx}',
+	],
+  prefix: "",
+  theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
     },
-  });
-};
+    extend: {
+      colors: {
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
+        },
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
+    },
+  },
+  plugins: [require("tailwindcss-animate")],
+} satisfies Config
+
+export default config
